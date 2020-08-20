077bfe895c2da10c2faf0771e4d394f8d2f36371
Andre Postiga <andre.postiga@msn.com>
2020-08-20 08:48:45 -0300

    chore: add rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,6 @@ module.exports = {
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'import/prefer-default-export': 'off',
   },
 };
