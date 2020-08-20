a9fa6cb98b97fe9412ad0eae0124a02ff879e18b
Andre Postiga <andre.postiga@msn.com>
2020-08-20 08:49:30 -0300

    feat: create crud rules

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import PlanetController from './app/controllers/planetController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ hello: 'Hello World' });
 });
+
+routes.get('/api/v1/planets', PlanetController.index);
+routes.get('/api/v1/planets/:searchTerm', PlanetController.indexBySearchTerm);
+routes.post('/api/v1/planets', PlanetController.add);
+routes.delete('/api/v1/planets/:searchTerm', PlanetController.remove);
 
 export default routes;
