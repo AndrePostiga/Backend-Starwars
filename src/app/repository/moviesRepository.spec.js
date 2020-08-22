3b8634a95faa4fb02820ecd5475e1b0419e8cc34
Andre <andre.postiga@msn.com>
2020-08-22 16:55:22 -0300

    fix: moviesRepository test

--- src/app/repository/moviesRepository.spec.js
+++ src/app/repository/moviesRepository.spec.js
@@ -48,101 +48,105 @@ const makeSut = () => {
 };
 
 describe('Movies Repository', () => {
-  it('Should get movies by name', async () => {
-    // Arrange
-    const { sut } = makeSut();
+  describe('getMovies', () => {
+    it('Should get movies by name', async () => {
+      // Arrange
+      const { sut } = makeSut();
 
-    // act
-    const act = await sut.getMovies('Yavin IV');
+      // act
+      const act = await sut.getMovies('Yavin IV');
 
-    // assert
-    expect(act).toStrictEqual({
-      name: 'Yavin IV',
-      rotation_period: '24',
-      orbital_period: '4818',
-      diameter: '10200',
-      climate: 'temperate, tropical',
-      gravity: '1 standard',
-      terrain: 'jungle, rainforests',
-      surface_water: '8',
-      population: '1000',
-      residents: [],
-      films: ['http://swapi.dev/api/films/1/'],
-      created: '2014-12-10T11:37:19.144000Z',
-      edited: '2014-12-20T20:58:18.421000Z',
-      url: 'http://swapi.dev/api/planets/3/',
+      // assert
+      expect(act).toStrictEqual({
+        name: 'Yavin IV',
+        rotation_period: '24',
+        orbital_period: '4818',
+        diameter: '10200',
+        climate: 'temperate, tropical',
+        gravity: '1 standard',
+        terrain: 'jungle, rainforests',
+        surface_water: '8',
+        population: '1000',
+        residents: [],
+        films: ['http://swapi.dev/api/films/1/'],
+        created: '2014-12-10T11:37:19.144000Z',
+        edited: '2014-12-20T20:58:18.421000Z',
+        url: 'http://swapi.dev/api/planets/3/',
+      });
+    });
+
+    it('Should throw error if response breaks', () => {
+      const { sut, externalApiStub } = makeSut();
+      jest.spyOn(externalApiStub, 'get').mockImplementationOnce(() => {
+        throw new Error('AnyError');
+      });
+
+      const act = () => sut.getMovies('AnyMovie');
+
+      expect(act).rejects.toThrow(new Error('AnyError'));
+    });
+
+    it('Should return undefined if no data returns from external service ', async () => {
+      const { sut, externalApiStub } = makeSut();
+      jest.spyOn(externalApiStub, 'get').mockImplementationOnce(() => {
+        return { data: { count: 0, next: null, previous: null, results: [] } };
+      });
+
+      const act = await sut.getMovies('AnyMovie');
+
+      expect(act).toBe(undefined);
+    });
+
+    it('Should call getMovies with right planet name', async () => {
+      const { sut, externalApiStub } = makeSut();
+      const spyGetMovies = jest.spyOn(sut, 'getMovies');
+
+      const act = await sut.getMovies('anyPlanetName');
+
+      expect(spyGetMovies).toHaveBeenCalledWith('anyPlanetName');
     });
   });
 
-  it('Should throw error if response breaks', () => {
-    const { sut, externalApiStub } = makeSut();
-    jest.spyOn(externalApiStub, 'get').mockImplementationOnce(() => {
-      throw new Error('AnyError');
+  describe('getMoviesCount', () => {
+    it('Should count movies correctly', async () => {
+      const { sut } = makeSut();
+
+      const act = await sut.getMoviesCount('Yavin IV');
+
+      expect(act).toBe(1);
     });
 
-    const act = () => sut.getMovies('AnyMovie');
+    it('Should return zero if getMovies return undefined', async () => {
+      const { sut } = makeSut();
+      jest.spyOn(sut, 'getMovies').mockImplementationOnce(() => {
+        return undefined;
+      });
 
-    expect(act).rejects.toThrow(new Error('AnyError'));
-  });
+      const act = await sut.getMoviesCount('AnyMovie');
 
-  it('Should return undefined if no data returns from external service ', async () => {
-    const { sut, externalApiStub } = makeSut();
-    jest.spyOn(externalApiStub, 'get').mockImplementationOnce(() => {
-      return { data: { count: 0, next: null, previous: null, results: [] } };
+      expect(act).toBe(0);
     });
 
-    const act = await sut.getMovies('AnyMovie');
+    it('Should return zero if getMovies return null', async () => {
+      const { sut } = makeSut();
+      jest.spyOn(sut, 'getMovies').mockImplementationOnce(() => {
+        return null;
+      });
 
-    expect(act).toBe(undefined);
-  });
+      const act = await sut.getMoviesCount('AnyMovie');
 
-  it('Should call getMovies with right planet name', async () => {
-    const { sut, externalApiStub } = makeSut();
-    const spyGetMovies = jest.spyOn(sut, 'getMovies');
-
-    const act = await sut.getMovies('anyPlanetName');
-
-    expect(spyGetMovies).toHaveBeenCalledWith('anyPlanetName');
-  });
-
-  it('Should count movies correctly', async () => {
-    const { sut } = makeSut();
-
-    const act = await sut.getMoviesCount('Yavin IV');
-
-    expect(act).toBe(1);
-  });
-
-  it('Should return zero if getMovies return undefined', async () => {
-    const { sut } = makeSut();
-    jest.spyOn(sut, 'getMovies').mockImplementationOnce(() => {
-      return undefined;
+      expect(act).toBe(0);
     });
 
-    const act = await sut.getMoviesCount('AnyMovie');
+    it('Should throw error if getMovies return error', () => {
+      const { sut } = makeSut();
+      jest.spyOn(sut, 'getMovies').mockImplementationOnce(() => {
+        throw new Error('AnyError');
+      });
 
-    expect(act).toBe(0);
-  });
+      const act = () => sut.getMoviesCount('AnyMovie');
 
-  it('Should return zero if getMovies return null', async () => {
-    const { sut } = makeSut();
-    jest.spyOn(sut, 'getMovies').mockImplementationOnce(() => {
-      return null;
+      expect(act).rejects.toThrow(new Error('AnyError'));
     });
-
-    const act = await sut.getMoviesCount('AnyMovie');
-
-    expect(act).toBe(0);
-  });
-
-  it('Should throw error if getMovies return error', () => {
-    const { sut } = makeSut();
-    jest.spyOn(sut, 'getMovies').mockImplementationOnce(() => {
-      throw new Error('AnyError');
-    });
-
-    const act = () => sut.getMoviesCount('AnyMovie');
-
-    expect(act).rejects.toThrow(new Error('AnyError'));
   });
 });
